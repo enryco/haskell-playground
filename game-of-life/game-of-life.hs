d4028265a3ba7d226948a086d707fa04a651914a
Enrico Scherlies <e.scherlies@me.com>
2020-08-06 15:17:31 +0200

    generalize gridsize as param

--- game-of-life/game-of-life.hs
+++ game-of-life/game-of-life.hs
@@ -1,16 +1,15 @@
 import Data.List
 import Control.Concurrent
 
-type Pos = (Int,Int)
-type Colony = [Pos]
+type Pos        = (Int,Int)
+type GridSize   = Pos
+type Colony     = [Pos]
 
 
-grid_size = 60 :: Int
-
-sample_grid   = [(1 :: Int,1 :: Int),(0,3),(1,2),(2,2),(1,3),(0,1)]
-sample_grid2  = [(1 :: Int,1 :: Int),(1,2), (2,1), (1,3),(0,2)]
-sample_grid3  = [(x :: Int,5 :: Int) | x <- [0..9]]
-glider        = [(3 :: Int,2 :: Int), (4,3), (5,3),(4,4),(3,4)]
+sample_grid  = [(1,1),(0,3),(1,2),(2,2),(1,3),(0,1)] :: Colony
+sample_grid2 = [(1,1),(1,2), (2,1), (1,3),(0,2)] :: Colony
+sample_grid3 = [(x,5) | x <- [0..9]] :: Colony
+glider       = [(3,2), (4,3), (5,3),(4,4),(3,4)] :: Colony
 
 ------------------
 -- COMPUTATIONS --
@@ -19,13 +18,14 @@ glider        = [(3 :: Int,2 :: Int), (4,3), (5,3),(4,4),(3,4)]
 inc :: Int -> Int
 inc = (+1)
 
-dontFallOffTheGrid :: Pos -> Pos
-dontFallOffTheGrid (x,y) = (putBackInside x, putBackInside y) where
-  putBackInside coordinate = (coordinate + grid_size) `mod` grid_size
+putBack :: GridSize -> Pos -> Pos
+putBack (width, height) (x,y) = ((x + width) `mod` width, (y + height) `mod` height)
 
+dontFallOffTheGrid :: GridSize -> [Pos] -> [Pos]
+dontFallOffTheGrid gridSize = map (putBack gridSize)
 
-getNextToPos :: Pos -> Colony
-getNextToPos (x,y) = [dontFallOffTheGrid (x+offset_x,y+offset_y) | 
+getNextToPos :: Pos -> [Pos]
+getNextToPos (x,y) = [(x+offset_x,y+offset_y) | 
   offset_x <- [-1..1],
   offset_y <- [-1..1],
   (offset_x,offset_y) /= (0,0)]
@@ -35,8 +35,8 @@ potentialsOf :: Colony -> Colony
 potentialsOf []     = []
 potentialsOf colony =  getNextToPos (head colony) ++ potentialsOf (tail colony)
 
-accumulatedPotentialsOff :: Colony -> [[Pos]]
-accumulatedPotentialsOff colony = group (sort (potentialsOf colony))
+accumulatedPotentialsOff :: GridSize -> Colony -> [[Pos]]
+accumulatedPotentialsOff gridSize colony = group (sort (dontFallOffTheGrid gridSize (potentialsOf colony)))
 
 countPotentials :: [[Pos]] -> [(Pos,Int)]
 countPotentials [] = []
@@ -48,8 +48,8 @@ birthOrAlive cs ((p,n):pots) | p `elem` cs && n >= 2 && n <= 3 = p:birthOrAlive 
                              | not (elem p cs) && n == 3       = p:birthOrAlive cs pots
                              | otherwise = birthOrAlive cs pots
 
-nextGen :: Colony -> Colony
-nextGen colony = birthOrAlive colony (countPotentials (accumulatedPotentialsOff colony))
+nextGen :: GridSize -> Colony -> Colony
+nextGen gridSize colony = birthOrAlive colony (countPotentials $ accumulatedPotentialsOff gridSize colony)
 
 ----------------
 -- IO ACTIONS --
@@ -72,27 +72,32 @@ writeatWithOffset (ox, oy) xs (x,y) = writeat xs (x+ox, y+oy)
 goto :: Pos -> IO ()
 goto (x,y) = putStr ("\ESC[" ++ show y ++ ";" ++ show x ++ "H" )
 
-putFrame :: IO ()
-putFrame = do
+putFrame :: GridSize -> IO ()
+putFrame (width, height)= do
   goto (1,1)
-  putStrLn $ "╔" ++ replicate grid_size '═' ++ "╗"
-  goto (1,grid_size+2)
-  putStrLn $ "╚" ++ replicate grid_size '═' ++ "╝"
-  sequence_ [writeat "║" (x,y)| y <- [2..(grid_size+1)], x <- [1,grid_size+2]]
+  putStrLn $ "╔" ++ replicate width '═' ++ "╗"
+  goto (1,height+2)
+  putStrLn $ "╚" ++ replicate height '═' ++ "╝"
+  sequence_ [writeat "║" (x,y)| y <- [2..(height+1)], x <- [1,width+2]]
 
-run :: Int -> Colony -> Colony -> IO ()
-run iteration initialColony initialColony' = do
+run :: GridSize -> Int -> Colony -> Colony -> IO ()
+run gridSize iteration initialColony initialColony' = do
+  let (_, height) = gridSize
   cls
-  putFrame
+  putFrame gridSize
   putGrid initialColony
-  goto (1, grid_size + 3)
-  putStrLn $ "Iteration: " ++ show iteration ++ " | Gridsize: " ++ show grid_size
+  goto (1, height + 3)
+  putStrLn $ "Iteration: " ++ show iteration ++ " | Gridsize: " ++ show gridSize
   putStrLn $ show initialColony'
-  threadDelay 100000
-  run (inc iteration) (nextGen initialColony) initialColony'
+  threadDelay (1000000 `div` 60)
+  run gridSize (inc iteration) (nextGen gridSize initialColony) initialColony'
 
-start :: Colony -> IO ()
-start c = run 0 c c
+initiate :: GridSize -> Colony -> IO ()
+initiate gridSize c = run gridSize 0 c c
+
+getGridSize :: String -> GridSize
+getGridSize ""  = (10,10)
+getGridSize str = read str :: GridSize
 
 ----------
 -- main --
@@ -102,6 +107,9 @@ main :: IO ()
 main = do
   putStrLn "Select sample grid [1..4]"
   c <- getChar
+  putStrLn "Specify GridSize: (width,height)"
+  line <- getLine
+  let start = initiate $ getGridSize line
   case c of
     '1' -> start glider
     '2' -> start sample_grid
